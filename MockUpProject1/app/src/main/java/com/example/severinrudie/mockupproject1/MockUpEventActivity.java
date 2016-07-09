93b83665d64293eaf85b23da7a8865ae420f8989
Baron-Severin <severinrudie@gmail.com>
2016-07-09 15:49:36 -0700

    early mockup created

---
+++ MockUpProject1/app/src/main/java/com/example/severinrudie/mockupproject1/MockUpEventActivity.java
@@ -0,0 +1,13 @@
+package com.example.severinrudie.mockupproject1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MockUpEventActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mock_up_event);
+    }
+}
