c6a5cd39e0e9e40d7385e7d5e8a70bedd64fc60d
Baron-Severin <severinrudie@gmail.com>
2016-07-09 23:45:49 -0700

    setting up main project

---
+++ EventOrganizer/app/src/main/java/com/rudie/severin/eventorganizer/EventsActivity.java
@@ -0,0 +1,16 @@
+package com.rudie.severin.eventorganizer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EventsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_events);
+
+
+
+    }
+}
