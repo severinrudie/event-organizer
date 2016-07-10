f104923a6dc55f3740c254aec2924a385d79179f
Baron-Severin <severinrudie@gmail.com>
2016-07-09 17:05:08 -0700

    mockup expanded

---
+++ MockUpProject1/app/src/main/java/com/example/severinrudie/mockupproject1/MockUpDetailActivity.java
@@ -0,0 +1,13 @@
+package com.example.severinrudie.mockupproject1;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MockUpDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mock_up_detail);
+    }
+}
--- MockUpProject1/app/src/main/java/com/example/severinrudie/mockupproject1/MockUpEventActivity.java
+++ MockUpProject1/app/src/main/java/com/example/severinrudie/mockupproject1/MockUpEventActivity.java
@@ -1,13 +1,28 @@
 package com.example.severinrudie.mockupproject1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class MockUpEventActivity extends AppCompatActivity {
+
+    LinearLayout myEvent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mock_up_event);
+
+        myEvent = (LinearLayout) findViewById(R.id.mockupEvent);
+        myEvent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MockUpDetailActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
